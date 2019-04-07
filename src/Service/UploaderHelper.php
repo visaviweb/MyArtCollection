e276d934ba3e98b29fa88c6e31747587892a38ee
Olivier Wailly <olivier@visaviweb.com>
2019-04-07 18:32:10 +0200

    added flysystem

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -5,50 +5,54 @@ namespace App\Service;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Asset\Context\RequestStackContext;
+use League\Flysystem\FilesystemInterface;
 
 class UploaderHelper
 {
-    private $uploadBasePath;
+    private $filesystem;
     private $requestStackContext;
-    const UPLOAD_DIR = 'upload';
+    private $publicAssetBaseUrl;
     
-    public function __construct(string $uploadBasePath, RequestStackContext $requestStackContext)
+    public function __construct(FilesystemInterface $uploadFilesystem, RequestStackContext $requestStackContext, string $uploadedAssetsBaseUrl )
     {
-        $this->uploadBasePath = $uploadBasePath;
+        $this->filesystem = $uploadFilesystem;
         $this->requestStackContext = $requestStackContext;
+        $this->publicAssetBaseUrl = $uploadedAssetsBaseUrl;
     }
     
     public function moveUploadedImage(UploadedFile $uploadedFile): string
     {
         $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
         $newFilename = $originalFilename.'_U_'.uniqid().'_.'.$uploadedFile->guessExtension();
-        $uploadedFile->move(
-            $this->getUploadPath(),
-            $newFilename
+        $stream = fopen($uploadedFile->getPathname(), 'r');
+        $result = $this->filesystem->writeStream(
+            $newFilename,
+            $stream
         );
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
+        if ($result === false) {
+            throw new \Exception(sprintf('Could not write uploaded file "%s"', $newFilename));
+        }
+        
         return $newFilename;
     }
 
     public function getPublicPath(string $filename) : string
     {
         return $this->requestStackContext
-            ->getBasePath().'/'.self::UPLOAD_DIR.'/'.$filename;
+            ->getBasePath().$this->publicAssetBaseUrl.'/'.$filename;
     }
 
     public function getUploadedFilesList()
     {
-        $finder = new Finder();
-        return $finder->files()->in($this->getUploadPath());
+        
+        return $this->filesystem->listContents('', false);
     }
 
-    public function hasUnregisteredImages() : boolean
+    public function hasUnregisteredImages() : bool
     {
-        $finder = new Finder();
-        return \count($finder->files()->in($this->getUploadPath())) > 0;
-    }
-
-    private function getUploadPath() : string
-    {
-        return $this->uploadBasePath.'/'.self::UPLOAD_DIR;
+        return \count($this->filesystem->listContents('', false)) > 0;
     }
 }
